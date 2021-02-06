630669a262e6c9cb87a4cfd627c8e8b4e241b1e5
Harsh Parekh <harsh_parekh@outlook.com>
2021-02-06 08:33:49 -0500

    Hide status from title.
    You can pass -E 1 to show the extended title.

--- config.def.h
+++ config.def.h
@@ -1,5 +1,6 @@
 /* modifier 0 means no modifier */
 static int surfuseragent    = 1;  /* Append Surf version to default WebKit user agent */
+static int extendedtitle    = 0;  /* 0 to not append surf's toggle and page status to title. */
 static char *fulluseragent  = ""; /* Or override the whole user agent string */
 static char *scriptfile     = "~/.config/surf/script.js";
 static char *styledir       = "~/.config/surf/styles/";
@@ -190,6 +191,7 @@ static Key keys[] = {
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_a,      togglecookiepolicy, { 0 } },
 	{ 0,                     GDK_KEY_F11,    togglefullscreen, { 0 } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_o,      toggleinspector, { 0 } },
+	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_e,      toggletitle,        { 0 } },
 
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_c,      toggle,     { .i = CaretBrowsing } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_f,      toggle,     { .i = FrameFlattening } },
--- surf.c
+++ surf.c
@@ -234,6 +234,7 @@ static void toggle(Client *c, const Arg *a);
 static void togglefullscreen(Client *c, const Arg *a);
 static void togglecookiepolicy(Client *c, const Arg *a);
 static void toggleinspector(Client *c, const Arg *a);
+static void toggletitle(Client *c, const Arg *a);
 static void find(Client *c, const Arg *a);
 
 /* Buttons */
@@ -649,13 +650,19 @@ updatetitle(Client *c)
 		gettogglestats(c);
 		getpagestats(c);
 
-		if (c->progress != 100)
-			title = g_strdup_printf("[%i%%] %s:%s | %s",
-			        c->progress, togglestats, pagestats, name);
-		else
-			title = g_strdup_printf("%s:%s | %s",
-			        togglestats, pagestats, name);
-
+		if (c->progress != 100) {
+            if (!extendedtitle)
+                title = g_strdup_printf("[%i%%] %s", c->progress, name);
+            else
+			    title = g_strdup_printf("[%i%%] %s:%s | %s",
+						c->progress, togglestats, pagestats, name);
+        } else {
+            if (!extendedtitle)
+                title = g_strdup_printf("%s", name);
+            else
+			    title = g_strdup_printf("%s:%s | %s",
+						togglestats, pagestats, name);
+        }
 		gtk_window_set_title(GTK_WINDOW(c->win), title);
 		g_free(title);
 	} else {
@@ -1954,6 +1961,13 @@ toggleinspector(Client *c, const Arg *a)
 }
 
 void
+toggletitle(Client *c, const Arg *a)
+{
+	extendedtitle = !extendedtitle;
+	updatetitle(c);
+}
+
+void
 find(Client *c, const Arg *a)
 {
 	const char *s, *f;
@@ -2042,6 +2056,9 @@ main(int argc, char *argv[])
 	case 'e':
 		embed = strtol(EARGF(usage()), NULL, 0);
 		break;
+    case 'E':
+        extendedtitle = strtol(EARGF(usage()), NULL, 0);
+        break;
 	case 'f':
 		defconfig[RunInFullscreen].val.i = 0;
 		defconfig[RunInFullscreen].prio = 2;
